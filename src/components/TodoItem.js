e8e8a530b51a14dd2ca740f81f7f245bab4f8df3
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-07-01 21:00:36 +0900

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,13 +5,13 @@ import { StButton, StButtonSet, StCard, StTitle } from '../style/MainPageStyle'
 function TodoItem({ item, handleDelete, handleToggle }) {
     return (
         <div>
-            <StCard key={item.id}>
+            <StCard>
                 <Link to={`/detailpage/${item.id}`} > 상세보기 </Link>
                 <StTitle>{item.title}</StTitle>
                 <p>{item.contents}</p>
                 <StButtonSet>
-                    <StButton onClick={() => handleToggle(item.id)} borderColor={"green"}>{item.success ? '취소' : '완료!'}</StButton>
-                    <StButton onClick={() => handleDelete(item.id)} borderColor={"red"}>삭제</StButton>
+                    <StButton onClick={() => handleToggle(item.id)} bordercolor={"green"}>{item.success ? '취소' : '완료!'}</StButton>
+                    <StButton onClick={() => handleDelete(item.id)} bordercolor={"red"}>삭제</StButton>
                 </StButtonSet>
             </StCard>
         </div>
